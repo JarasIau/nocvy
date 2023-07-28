a3e04e4fad718943be4c6b1590fdf737ecd75d17
JarasIau <jaraslau@tutanota.com>
2023-07-29 02:16:59 +0300

    Improved naming and various descriptions

--- noczwy.py
+++ noczwy.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 #NOCZWY v0.11
 #Copyright (C) 2022  Jaraslau
-#Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+#Use of this source code is governed by a BSD-style license that can be found in the license file.
 
 import argparse
 import urllib3
 import threading
 import queue
 
-def return_args():
-    parser = argparse.ArgumentParser(description="noczwy-mt - enumerarte website's directories and write to standard output", epilog="This software is licensed under the BSD-3-Clause license.")
-    parser.add_argument("-u", "--url", type=str, required=True, help="url to begin bruteforcing process (NO PROTOCOL, e.g. \"url.com\")")
-    parser.add_argument("-w", "--wordlist", type=str, required=True, help="point NOCZWY to a wordlist (Try wl.txt)")
+def get_args():
+    parser = argparse.ArgumentParser(description="noczwy - enumerate a url and write to standard output", epilog="This software is licensed under the BSD-3-Clause license.")
+    parser.add_argument("-u", "--url", type=str, required=True, help="url to enumerate (NO PROTOCOL, e.g. \"example.com\")")
+    parser.add_argument("-w", "--wordlist", type=str, required=True, help="path to a wordlist")
     parser.add_argument("-t", "--threads", type=int, default=12, help="a number of threads to use")
-    parser.add_argument("-m", "--head", action="store_true", help="use HEAD method instead of GET")
+    parser.add_argument("--head", action="store_true", help="use HEAD method instead of GET")
     return parser.parse_args()
 
 def form_queue(path):
     target_queue = queue.Queue()
-    with open(path, "r", encoding = "UTF-8") as raw_wordlist:
-        for line in raw_wordlist:
-            target_queue.put(f"/{line}".strip()) # This needs better readability and automatic url normalization
+    with open(path, "r", encoding = "UTF-8") as wordlist:
+        for line in wordlist:
+            target_queue.put(f"/{line}".strip()) #Needs automatic url normalization
     return target_queue
 
 def enumerate_dirs(connection_pool, method, target_queue, response_queue):
@@ -31,7 +31,7 @@ def enumerate_dirs(connection_pool, method, target_queue, response_queue):
         target_queue.task_done()
 
 def main():
-    args = return_args()
+    args = get_args()
     args.threads = 1 if args.threads < 1 else args.threads
     method = "HEAD" if args.head else "GET"
     target_queue = form_queue(args.wordlist)
